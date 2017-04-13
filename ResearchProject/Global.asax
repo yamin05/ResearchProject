ab97240d09ed08ab113356572513818aad8c00b1
yamin05 <yams.stj@gmail.com>
2017-04-13 20:44:30 +1000

    Startup Application

---
+++ ResearchProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResearchProject.MvcApplication" Language="C#" %>
---
+++ ResearchProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResearchProject.Startup))]
+namespace ResearchProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
